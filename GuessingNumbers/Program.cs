e8f978425a4d72010735c1e6750862f3b675fb41
fedewagner <federico.j.wagner@gmail.com>
2025-08-06 22:34:14 +0200

    missing constants were introduced!

--- GuessingNumbers/Program.cs
+++ GuessingNumbers/Program.cs
@@ -13,10 +13,15 @@ namespace GuessingNumbers
                 $"In this game you have to guess a number between 1 and 100. You have {MAX_GUESSES_ALLOWED} allowed guesses!");
 
             // Find out how to generate random numbers in C#
+            
             Random random = new Random();
-            int the_number = random.Next(1, 101);
+            const int MIN_FOR_RANDOM_FUNCTION = 1;
+            const int MAX_FOR_RANDOM_FUNCTION = 101;
+            int the_number = random.Next(MIN_FOR_RANDOM_FUNCTION, MAX_FOR_RANDOM_FUNCTION);
             int user_guess;
+
             int guesses_counter = 0;
+            
             int guessing_difference = 100; // If I don't initialize, then I got some Warning because of line 80:(
             int guesses_left;
             List<int> guessed_numbers = new List<int>();
@@ -26,7 +31,7 @@ namespace GuessingNumbers
             {
                 // Read user text & Convert to ints
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("So please enter a number between 1 and 100:");
+                Console.WriteLine($"So please enter a number between {MIN_FOR_RANDOM_FUNCTION} and {MAX_FOR_RANDOM_FUNCTION-1}:");
                 Console.ForegroundColor = ConsoleColor.White;
                 user_guess = Convert.ToInt32(Console.ReadLine());
                 guessed_numbers.Add(user_guess);
@@ -46,7 +51,7 @@ namespace GuessingNumbers
                     Console.WriteLine("Your guess is lower than the number!");
                     ++guesses_counter;
                     guesses_left = MAX_GUESSES_ALLOWED - guesses_counter;
-                    if (guessing_difference <= 5 && guesses_left >= 1)
+                    if (guessing_difference <= MAX_GUESSES_ALLOWED && guesses_left >= 1)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Your guess is very close! The guess is only 5 off or less");
@@ -63,7 +68,7 @@ namespace GuessingNumbers
                     Console.WriteLine("Your guess is higher than the number!");
                     ++guesses_counter;
                     guesses_left = MAX_GUESSES_ALLOWED - guesses_counter;
-                    if (guessing_difference <= 5 && guesses_left >= 1)
+                    if (guessing_difference <= MAX_GUESSES_ALLOWED && guesses_left >= 1)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Your guess is very close! The guess is only 5 off or less");
