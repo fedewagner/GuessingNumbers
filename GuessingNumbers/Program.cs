cbd5c5f4b36176a7c724eacfed442ebebd189e67
fedewagner <federico.j.wagner@gmail.com>
2025-08-06 23:23:29 +0200

    Included improvement for Michael's recommendation "What do you think about utilizing this and check for already guessed numbers? 😀 Like if the user already have entered a guess, notify him that he did already and do not count as a guess? 🤔"

--- GuessingNumbers/Program.cs
+++ GuessingNumbers/Program.cs
@@ -13,7 +13,7 @@ namespace GuessingNumbers
                 $"In this game you have to guess a number between 1 and 100. You have {MAX_GUESSES_ALLOWED} allowed guesses!");
 
             // Find out how to generate random numbers in C#
-            
+
             Random random = new Random();
             const int MIN_FOR_RANDOM_FUNCTION = 1;
             const int MAX_FOR_RANDOM_FUNCTION = 101;
@@ -21,7 +21,7 @@ namespace GuessingNumbers
             int userGuess;
 
             int guessesCounter = 0;
-            
+
             int guessingDifference = 100; // If I don't initialize, then I get some error:(
             int guessesLeft;
             List<int> guessedNumbers = new List<int>();
@@ -31,15 +31,28 @@ namespace GuessingNumbers
             {
                 // Read user text & Convert to ints
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine($"So please enter a number between {MIN_FOR_RANDOM_FUNCTION} and {MAX_FOR_RANDOM_FUNCTION-1}:");
-                Console.ForegroundColor = ConsoleColor.White;
-                userGuess = Convert.ToInt32(Console.ReadLine());
-                guessedNumbers.Add(userGuess);
+                Console.WriteLine(
+                    $"So please enter a number between {MIN_FOR_RANDOM_FUNCTION} and {MAX_FOR_RANDOM_FUNCTION - 1}:");
                 
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                
+                //Check if the value is already in the List
+                //if is in the list then print a message and ask again for a number
+                while (guessedNumbers.Contains(userGuess))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"You already tried with the number {userGuess}, therefore it doesn't count");
+                    Console.WriteLine(
+                        $"So please enter a newnumber between {MIN_FOR_RANDOM_FUNCTION} and {MAX_FOR_RANDOM_FUNCTION - 1}:");
+                    userGuess = Convert.ToInt32(Console.ReadLine());
+                }
+                //if is not in the list then add the number into the list and continue
+                guessedNumbers.Add(userGuess);
+                Console.ForegroundColor = ConsoleColor.White;
                 // calculate the difference between guesses
                 guessingDifference = int.Abs(theNumber - userGuess);
                 //can I do this with Enumerable.Range???
-                
+
                 if (guessingDifference == 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -54,6 +67,7 @@ namespace GuessingNumbers
                 {
                     Console.WriteLine("Your guess is higher than the number!");
                 }
+
                 ++guessesCounter;
                 guessesLeft = MAX_GUESSES_ALLOWED - guessesCounter;
                 if (guessingDifference <= MAX_GUESSES_ALLOWED && guessesLeft >= 1)
@@ -67,7 +81,6 @@ namespace GuessingNumbers
                 {
                     Console.WriteLine($"You still have {guessesLeft} guesses left!");
                 }
-                
             }
 
             if (guessingDifference != 0)
